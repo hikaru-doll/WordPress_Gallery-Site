f2a913b1d533c7b34de56a2865f25844f25cd32b
hikaru <makinaminami@gmail.com>
2026-08-11 10:20:14 +0900

    attentionを追加

--- footer.php
+++ footer.php
@@ -9,6 +9,10 @@
 
       <?php echo do_shortcode('[contact-form-7 id="604bdc8" title="Email-address"]'); ?>
     </div>
+    <div class="attention">
+      <p>本サイトは架空のクリエイターを想定して制作したポートフォリオ作品です。実在するクリエイターではありません。</p>
+      <p>アクセサリー画像は、スマホ撮影です（一部<a href="https://minne.com/@hikaridoll" target="_blank" rel="noopener noreferrer">minne🔗</a> にて販売中）。</p>
+    </div>
     <p class="copyright">&copy;<span id="year"></span> Hikaru doll Gallery-Site</p>
   </footer>
   <?php wp_footer(); ?>
