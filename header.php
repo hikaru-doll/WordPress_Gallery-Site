a7da0a2cb169bb33b5cdee37a4799483f659d9f3
hikaru <makinaminami@gmail.com>
2026-09-12 11:58:25 +0900

    GTM設置

--- header.php
+++ header.php
@@ -8,13 +8,32 @@ defined('ABSPATH') || exit;
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="format-detection" content="telephone=no" />
-  <!-- 構造化データ ------------------------------->
-  <!-- /構造化データ end--------------------------->
-  <!-- Google Analyticsのタグを貼る場所 ----------->
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-NN6443KM');
+  </script>
+  <!-- End Google Tag Manager -->
   <?php wp_head(); ?>
 </head>
 
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NN6443KM"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <?php wp_body_open(); ?>
   <!-- header  ------------------------------------->
   <header class="header">
